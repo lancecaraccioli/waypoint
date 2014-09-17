53e37bfccb242f5d42825079dff11f99e5aea552
Lance Caraccioli <lance.caraccioli@gmail.com>
2014-09-17 11:14:04 -0500

    added debug info to about screen

--- src/app/about/about-controller.js
+++ src/app/about/about-controller.js
@@ -1,9 +1,27 @@
 (function () {
   'use strict';
+
   angular.module('app').
     controller('AboutController', [
       '$scope',
-      function ($scope) {
+      '$window',
+      function ($scope, $window) {
+        var that = this,
+          win = angular.element($window);
+
+        var onResize = function windowResizeHandler() {
+          that.screen = {
+            height: win.height(),
+            width: win.width()
+          };
+        };
+
+        win.on('resize', onResize);
+
+        onResize();
+
+        $scope.$on('$destroy', function() { win.off('resize', onResize); });
+
       }
     ]);
 })();
